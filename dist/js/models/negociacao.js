55131fb8218a85df10751aef000f896e9b9ee0d9
helton-quintans <helton.quit@gmail.com>
2023-06-30 14:48:08 -0300

    chore: add typescript

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -6,6 +6,22 @@ export class Negociacao {
     constructor(data, quantidade, valor) {
         this.#data = data;
         this.#quantidade = quantidade;
-        this.#valor = valor
+        this.#valor = valor;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get quantidade() {
+        return this.#quantidade;
+    }
+
+    get valor() {
+        return this.#valor;
+    }
+
+    get volume() {
+        return this.#quantidade * this.#valor;
     }
 }
